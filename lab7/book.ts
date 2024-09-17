b580703086c4781bbf275bb9835db0a873a43190
Eli OCCANSEY <elioccansey@gmail.com>
2024-09-17 09:43:17 -0500

    model, route, controller added

---
+++ lab7/book.ts
@@ -0,0 +1,54 @@
+import { Author } from "./Author";
+
+const books: Book[] = []
+export class Book {
+    private id: string;
+    private title: string;
+    private isbn: string;
+    private publishedDate: Date;
+    private author: Author
+
+    constructor(
+        id: string,
+        title: string,
+        isbn: string,
+        publishedDate: Date,
+        author: Author
+    ) {
+        this.id = id;
+        this.title = title;
+        this.isbn = isbn;
+        this.publishedDate = publishedDate;
+        this.author = author;
+
+    }
+
+    save() {
+        books.push(this)
+        return this
+    }
+
+    update(): Book {
+        const index = books.findIndex(book => book.id === this.id)
+        books[index] = this;
+        return this;
+    }
+
+    static fetchAll(): Book[] {
+        return books;
+    }
+
+    static findById(id: string): Book {
+        const book = books.find(book => book.id === id)
+        if (book) return book;
+        throw new Error(`Book of id ${id} not found`);
+    }
+
+    static deleteById(id: string) {
+        const index = books.findIndex(book => book.id === id)
+        if (index > -1) books.splice(index, 1)
+        else throw new Error(`Book of id ${id} not found`);
+    }
+
+
+}
